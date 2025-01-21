2db8384dace1a9dd55c169e8a1710b366dcf8e05
AndreCandidoDev <andd68@hotmail.com>
2025-01-21 14:42:42 -0300

    Removendo arquivos desnecessarios

--- src/hooks/core.ts
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-
-export const useStore = create((set, get) => ({
-    count: 1,
-    inc: () => set((state: any) => ({ count: state.count + 1 })),
-}))
--- src/interfaces/pageRepos.i.ts
+++
@@ -1,3 +0,0 @@
-export interface PageReposProps {
-    hasSearch?: boolean
-}
